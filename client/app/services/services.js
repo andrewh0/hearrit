a78ccfa0c29ef5b2773cddbd7f89473d621a9d11
Andrew H <aho338@gmail.com>
2016-04-04 16:44:52 -0700

    Add Recommends factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -6,6 +6,7 @@ angular.module('hearrit.services', [])
       url: '/api/search',
       data: {query: {q: query}}
     }).then(function successCallback(response) {
+      console.log(response);
       return response.data;
     }, function errorCallback(response) {
       console.log('Error searching SoundCloud:', response);
@@ -15,3 +16,20 @@ angular.module('hearrit.services', [])
     searchSoundCloud: searchSoundCloud
   }
 })
+.factory('Recommends', function($http) {
+  var recommendTrack = function(trackID) {
+    return $http({
+      method: 'POST',
+      url: '/api/recommend',
+      data: {trackID: trackID}
+    }).then(function successCallback(response) {
+      console.log(response);
+      return response.data;
+    }, function errorCallback(response) {
+      console.log('Error recommending track: ', response);
+    })
+  };
+   return {
+     recommendTrack: recommendTrack
+   }
+});
