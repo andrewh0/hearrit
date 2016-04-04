eb449d7e463e06e74abf2b70b8feca5895cd51c9
Andrew H <aho338@gmail.com>
2016-04-04 16:04:08 -0700

    Add body-parser and add route for POSTing to SoundCloud

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var SC = require('node-soundcloud');
 var keys = require('./config');
+var bodyParser = require('body-parser');
 
 SC.init({
   id: keys.clientID,
@@ -10,9 +11,10 @@ SC.init({
 
 var app = express();
 
-
 app.listen(8000);
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // SC.get('/resolve', {
 //   url: 'https://soundcloud.com/kanyewest/ultralight-beam'
@@ -21,25 +23,28 @@ app.use(express.static('client'));
 // });
 
 app.get('/', function(req, res) {
-  SC.get('/tracks', {q: 'buskers'}, function(err, track) {
-    if (err) {
-      console.log('err', err);
-      // resjson(err);
-    } else {
-      // res.json(track);
-      res.render('index');
-    }
-  });
+  res.render('index');
+  // SC.get('/tracks', {q: 'buskers'}, function(err, track) {
+  //   if (err) {
+  //     console.log('Err', err);
+  //     // resjson(err);
+  //   } else {
+  //     // res.json(track);
+  //
+  //   }
+  // });
 });
 
-app.get('/songs', function(req, res) {
-  SC.get('/tracks', {q: 'buskers'}, function(err, track) {
+app.post('/api/search', function(req, res) {
+  console.log(req.body);
+  SC.get('/tracks', req.body.query, function(err, track) {
     if (err) {
       console.log('err', err);
       // resjson(err);
     } else {
       // res.json(track);
-      res.render('index');
+      // res.render('index');
+      res.json(track);
     }
   });
 });
