863d5bcb107f58d3afb700bcc85dcf5fa728d138
Andrew H <aho338@gmail.com>
2016-04-04 11:53:20 -0700

    Integrate SoundCloud SDK

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 var express = require('express');
+var SC = require('node-soundcloud');
+var keys = require('./config');
+
+SC.init({
+  id: keys.clientID,
+  secret: keys.clientSecret,
+  uri: 'http://example.com/callback'
+})
 
 var app = express();
+
+// app.use(__dirname + '/client/');
 
 app.listen(8000);
 
 app.get('/', function(req, res) {
-  res.send('hello');
+  SC.get('/tracks/13158665', function(err, track) {
+    if (err) {
+      console.log('err', err);
+      // resjson(err);
+    } else {
+      res.json(track);
+    }
+  });
 });
 
 module.exports = app;
